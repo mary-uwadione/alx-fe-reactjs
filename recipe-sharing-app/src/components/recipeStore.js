82804727380102c72057dcee5f7a76f9324acead
Luxar <237206419+mary-uwadione@users.noreply.github.com>
2026-01-25 20:48:43 +0100

    add recipeStore file

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,12 +1,26 @@
-import { create } from 'zustand';
+import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
+
+  addRecipe: (newRecipe) =>
     set((state) => ({
-      recipes: [...state.recipes, recipe],
+      recipes: [...state.recipes, newRecipe],
     })),
-     setRecipes: (recipes) => set({ recipes })
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
